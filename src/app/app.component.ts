f7e00c948a8dcbd43572cdfe4d6e9b37d4f1e9ba
soul-bone <soulbone@126.com>
2017-05-21 15:42:52 +0800

    delete some content not important

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-import { MetadataService } from 'ng2-metadata';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-bone',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  title = 'app works!';
-  constructor(private metadataService: MetadataService) { }
- 
-  ngOnInit() {
-    this.metadataService.setTitle(`app bone`);
-    this.metadataService.setTag('og:image', '');
-  }
+export class AppComponent{
+
 }
